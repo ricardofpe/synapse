d6cff3c859798eef74689ee8a84a905e354a86f6
ricardofpe <ricardofonseca_1@hotmail.com>
2025-01-29 21:17:32 -0300

    :sparkles: feat: Add connection with gpt-server

--- gpt-server/src/index.js
+++ gpt-server/src/index.js
@@ -1,30 +1,50 @@
-import express from 'express'
-import {createServer} from 'http'
-import {Server} from "socket.io"
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
-import {getLlama, LlamaChatSession} from "node-llama-cpp";
+import { getLlama, LlamaChatSession } from "node-llama-cpp";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const llama = await getLlama();
 const model = await llama.loadModel({
-    modelPath: path.join(__dirname, "models", "Meta-Llama-3-8B-Instruct.Q4_K_M.gguf")
+    modelPath: path.join(__dirname, "..", "models", "llama-2-7b.Q4_0.gguf"),
 });
 const context = await model.createContext();
 const session = new LlamaChatSession({
-    contextSequence: context.getSequence()
+    contextSequence: context.getSequence(),
 });
 
+const app = express();
+const server = createServer(app);
 
-const app = express()
-const server = createServer(app)
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        credentials: true,
+    },
+});
 
-const PORT = process.env.PORT || 8080
+io.on("connection", (soc) => {
+    console.log("There is a new connection");
+    soc.on("message", async (msg) => {
+        const bot_reply = await session.prompt(msg);
+        soc.emit("response", bot_reply);
+    });
+});
 
+const PORT = process.env.PORT || 8080;
 
-
-app.listen(PORT, () => {
-    console.log("Server running on port", PORT)
-})
+server.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
--- gpt-spa/src/App.jsx
+++ gpt-spa/src/App.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 import ChatMessage from "./components/ChatMessage";
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [inputMessage, setInputMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:8080");
+    setSocket(newSocket);
+
+    newSocket.on("response", (message) => {
+      setMessages([
+        ...messages,
+        {
+          type: "receive",
+          message,
+        },
+      ]);
+    });
+    return () => newSocket.close();
+  }, []);
+
+  const sendMessage = () => {
+    setMessages([
+      ...messages,
+      {
+        type: "send",
+        message: inputMessage,
+      },
+    ]);
+    socket.emit("message", inputMessage);
+  };
+
   return (
     <>
       <div className="p-5 h-screen bg-black">
@@ -13,11 +46,16 @@ function App() {
           </div>
           <div className="h-[100px] p-3 flex justify-center items-center bg-gray-700">
             <input
+              value={inputMessage}
+              onChange={(e) => setInputMessage(e.target.value)}
               type="text"
               placeholder="Type something..."
               className="w-full p-2 bg-transparent text-white border-white border-2 rounded-md outline-none"
             />
-            <button className="bg-violet-600 px-3 py-2 mx-2 rounded-md text-white cursor-pointer">
+            <button
+              onClick={sendMessage}
+              className="bg-violet-600 px-3 py-2 mx-2 rounded-md text-white cursor-pointer"
+            >
               Send
             </button>
           </div>
